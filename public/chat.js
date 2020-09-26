87d4c118a9cdd9a2ec3aed5d17868ec1d28b6c26
Sahil Hussain <sahil.hussain113@gmail.com>
2020-09-26 16:51:55 +0530

    SimpleChatApp using socket.io

---
+++ public/chat.js
@@ -0,0 +1,27 @@
+$(function(){
+    var socket = io.connect('http://localhost:3000')
+
+    //button and inputs
+    var message = $("#message")
+    var username = $("#username")
+    var send_message = $("#send_message")
+    var change_username = $("#change_username")
+    var chatroom = $("#chatroom")
+
+    //Emit message
+    send_message.click(() => {
+        socket.emit('new_message', {message: message.val()})
+    })
+
+    //Listen Message
+    socket.on("new_message", (data) => {
+        console.log(data)
+        chatroom.append("<p class='message'>"+ data.username+ " : " +data.message+"</p>")
+    })
+
+    //Emit a username
+    change_username.click(()=>{
+        console.log('Hi')
+        socket.emit('change_username', {username: username.val()})
+    })
+});
